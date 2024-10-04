b72ef0771865a5fa988bc4bce4835895c8ee2ee9
htl-leonding <t.stuetz@htl-leonding.ac.at>
2024-10-04 15:09:02 +0200

    2024-10-04

--- auth/generate_keys.sh
+++ auth/generate_keys.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 openssl genrsa -out rsaPrivateKey.pem 2048
-openssl rsa -pubout -in rsaPrivateKey.pem -out publicKey.pem
-openssl pkcs8 -topk8 -nocrypt -inform pem -in rsaPrivateKey.pem -outform pem -out privateKey.pem
+openssl rsa -pubout -in rsaPrivateKey.pem -out src/main/resources/pk/publicKey.pem
+openssl pkcs8 -topk8 -nocrypt \
+                 -inform pem \
+                 -in rsaPrivateKey.pem \
+                 -outform pem \
+                 -out src/main/resources/pk/privateKey.pem
+rm rsaPrivateKey.pem
